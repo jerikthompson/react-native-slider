ab2e6a6893d9c79f4ef1719a0f3157802c775fd3
tianxiao <>
2022-08-31 23:01:28 +0800

    no message

--- src/Slider.js
+++ src/Slider.js
@@ -297,7 +297,7 @@ export default class Slider extends PureComponent {
       style,
       trackStyle,
       thumbStyle,
-      ...otherProps,
+      ...otherProps
     } = props;
 
     return otherProps;
